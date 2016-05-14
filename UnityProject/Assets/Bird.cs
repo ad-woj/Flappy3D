71247daf7663da05e3d238e246af60e348191d51
ad-woj <mywirtualspace@gmail.com>
2016-05-14 23:47:40 +0200

    Added own collision detection && pipes textures && tweaks on flight mechanism

--- UnityProject/Assets/Bird.cs
+++ UnityProject/Assets/Bird.cs
@@ -1,88 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 
 public class Bird : MonoBehaviour {
 
-    private const float bounceTime = 0.8f;
+    private const float bounceTime = 1f;
+    private GameObject bird;
     private float speed;
     private Vector3 dir;
     private GameObject handleUpDir;
     private GameObject handleDownDir;
+    private CollisionDetector collisionDetector;
     private float lastBounceTime;
-    Vector3 jumpForce = new Vector3(0.0f, 7.3f, 0.0f);
+    private float bouncesSum;
+    bool goingUp;
 
 
     // Use this for initialization
     void Start () {
-        speed = 2f;
+        bird = GameObject.Find("BirdObject");
+        collisionDetector = GameObject.FindObjectOfType<CollisionDetector>();
+        speed = 4f;
         lastBounceTime = 0;
+        bouncesSum = 0;
+        goingUp = false;
         handleUpDir = GameObject.Find("Up");
         handleDownDir = GameObject.Find("Down");
         dir = handleDownDir.transform.position - transform.localPosition; // starting direction to bottom handle
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         float rotationModifier;
         float currentFrameStep = speed * Time.deltaTime;
-        float timeFromLastBounce;
-        bool goingUp = false;
 
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space)) {
             lastBounceTime = bounceTime;
+            bouncesSum += 1;
             goingUp = true;
         };
 
-        if (lastBounceTime > 0){ // Bird is going up after bounce
+        if (lastBounceTime > 0) { // Bird is going up after bounce
+            goingUp = true;
             dir = handleUpDir.transform.position - transform.localPosition;
             rotationModifier = 2.5f;
-            currentFrameStep *= 1.4f; 
-            lastBounceTime -= Time.deltaTime;
+            currentFrameStep *= 1.1f;
+            lastBounceTime -= Time.deltaTime * 1.2f;
         }
         else { // Bird is falling down
+            bouncesSum = 0;
             goingUp = false;
             rotationModifier = 1f;
             dir = handleDownDir.transform.position - transform.localPosition;
         }
 
-        transform.Translate(dir.normalized * currentFrameStep, Space.World);
         Quaternion targetRotation = Quaternion.LookRotation(dir);
 
-
-        
-            
-        //targetRotation = new Quaternion(targetRotation.x-0.1f, targetRotation.y, targetRotation.z, targetRotation.w);
-
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.02f * rotationModifier);
-
-        
-        if (goingUp){
-            Vector3 newRotationAngles = transform.rotation.eulerAngles;
-
-            newRotationAngles.x -= 5;
-
-            transform.rotation = Quaternion.Euler(newRotationAngles);
-        }
-        
-        float xAngle = transform.eulerAngles.x;
-        Debug.Log(xAngle);
-
-        /*
-        if (zAngle > 20 && zAngle <= 180)
+        if (goingUp)
         {
-            zAngle = 20;
+            targetRotation = Quaternion.Euler(targetRotation.eulerAngles.x + (40f - (bouncesSum * 2 % 20) * 4), 0, 0);
+            dir = new Vector3(handleUpDir.transform.position.x,
+                              handleUpDir.transform.position.y * (0.4f + lastBounceTime * 0.1f + (bouncesSum % 10) * 0.05f),
+                              handleUpDir.transform.position.z)
+                              - transform.localPosition;
         }
-        else if (zAngle < 340 && zAngle > 180)
-        {
-            zAngle = 340;
-        } */
-        
-        
+
+        transform.Translate(dir.normalized * currentFrameStep, Space.World); // Move
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.02f * rotationModifier); // Rotate
+
+        // Checking collisions, getting list of objects that collides with bird (usually it is one object
+        // but I prepared list in case there are multiple collided objects)
+        List<GameObject> collidedObjects = collisionDetector.checkCollsionsWith(GameObject.Find("BirdObject"));
+        if (collidedObjects.Count != 0)
+        { 
+            // TODO: Implement menu with restart game option && lives counter && points counter
+                Debug.Log("Collision!");
+                GameObject.Destroy(bird);
+        };
 
         updateDirectionObjectsPositions(currentFrameStep);
-        //Debug.Log(lastBounceTime);
     }
 
 
