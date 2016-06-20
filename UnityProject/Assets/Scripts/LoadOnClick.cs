b55bbc5e4b35acf98f153a6cdf2cc7875684131c
piotrkardys <piotrk@mobilewebpro.pl>
2016-06-20 21:16:13 +0200

    menu &authors scenes added

---
+++ UnityProject/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(int level)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }
+}
---
+++ UnityProject/Assets/Scripts/StartGameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class StartGameController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Button butonObj = GameObject.Find("Start").GetComponent<Button>();
+
+        butonObj.onClick.AddListener(delegate { startButtonClicked(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void startButtonClicked()
+    {
+        startGame();
+    }
+
+    public void startGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
